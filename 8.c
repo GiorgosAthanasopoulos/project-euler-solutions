f5bcb90266ece30e8fff038e0385cfad735d7f49
GiorgosAthanasopoulos <spzdoom@gmail.com>
2024-04-03 00:04:26 +0300

    Added pseudocode for 8 because of troubles with storing 1000 digit number

---
+++ 8.c
@@ -0,0 +1,29 @@
+int main() {
+  // only pseudocode because of troubles with storing a 1000 digit number
+  /*
+   * unsigned long long int number =
+   * 73167176531330624919225119674426574742355349194934...;
+   *
+   * int product = 0;
+   *
+   * int max = 0;
+   *
+   * for (int i = 0; i < number_of_digits(number) - 12; ++i) {
+   *
+   *   for (int j = 0; j < 13; ++j) {
+   *
+   *    product *= j;
+   *
+   *  }
+   *
+   *  if (product > max) {
+   *
+   *    max = product;
+   *
+   *  }
+   *
+   * }
+   * printf("%d\n", max);
+   */
+  return 0;
+}
