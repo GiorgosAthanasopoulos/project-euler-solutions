6753b8e497c08a0c6b2fd9de79b0312b97a195d9
GiorgosAthanasopoulos <spzdoom@gmail.com>
2024-04-09 19:48:36 +0300

    Added solution to problem 29

---
+++ 29.c
@@ -0,0 +1,25 @@
+#include <malloc.h>
+#include <math.h>
+int not_contains(int *sequence, int size, int n) {
+  for (int i = 0; i < size; ++i) {
+    if (sequence[i] == n) {
+      return 0;
+    }
+  }
+  return 1;
+}
+int main() {
+  int size = 100 * 100;
+  int *sequence = malloc(sizeof(int) * size);
+  int index = 0;
+  for (int a = 2; a <= 100; ++a) {
+    for (int b = 2; b <= 100; ++b) {
+      int temp = pow(a, b);
+      if (not_contains(sequence, size, temp)) {
+        sequence[index++] = temp;
+      }
+    }
+  }
+  printf("%d\n", index - 1);
+  return 0;
+}
