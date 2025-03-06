62d15a693ddef4300d8d4cafa72bec5199734203
GiorgosAthanasopoulos <spzdoom@gmail.com>
2025-03-06 23:01:40 +0200

    ADD: solution for prob 1

---
+++ src/0-100/0-20/1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv) {
+  printf("[INFO]: Problem: https://projecteuler.net/problem=1\n");
+  printf("[INFO]: Description: find the sum of all of the multiples of 3 and 5 "
+         "below the number provided as the first program argument\n");
+
+  if (argc < 2) {
+    fprintf(stderr, "[ERROR]: missing parameters: limit\n");
+    return 1;
+  }
+
+  int limit = atoi(argv[1]);
+  int sum = 0;
+
+  for (int i = 1; i < limit; ++i) {
+    if (i % 3 == 0 || i % 5 == 0) {
+      sum += i;
+    }
+  }
+
+  printf("[INFO]: Solution: %d\n", sum);
+  return 0;
+}
