05931d25424fb6a4f9fc0d24d887e9e12c57e6f4
GiorgosAthanasopoulos <spzdoom@gmail.com>
2025-03-07 00:11:12 +0200

    FIX: prob no3 solution

--- src/0-100/0-20/3.c
+++ src/0-100/0-20/3.c
@@ -2,6 +2,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int smallestFactor(long target) {
+  if (target <= 1) {
+    fprintf(
+        stderr,
+        "[ERROR]: smallestFactor argument must be a number greater than 1\n");
+    exit(1);
+  }
+  for (long i = 2, end = sqrt(target); i <= end; ++i) {
+    if (target % i == 0) {
+      return i;
+    }
+  }
+  return target;
+}
+
 int isPrime(long long needle) {
   for (long long i = 2; i <= sqrt(needle); ++i) {
     if (needle % i == 0 && needle != i) {
@@ -18,6 +33,9 @@ int main(int argc, char **argv) {
          "provided as the first program argument\n");
   printf("[INFO]: Reference: "
          "https://en.wikipedia.org/wiki/Prime_number\n");
+  printf("[INFO]: Reference: "
+         "https://github.com/nayuki/Project-Euler-solutions/blob/master/java/"
+         "p003.java?ts=4\n");
 
   if (argc < 2) {
     fprintf(stderr, "[ERROR]: missing argument: target number\n");
@@ -26,20 +44,15 @@ int main(int argc, char **argv) {
 
   long long target = atoll(argv[1]);
 
-  long long needle = target;
-
-  while (needle > 0) {
-    if (target % needle == 0 && isPrime(needle)) {
+  while (1) {
+    long factor = smallestFactor(target);
+    if (factor < target) {
+      target /= factor;
+    } else {
       break;
     }
-
-    needle--;
   }
 
-  if (needle != 0) {
-    printf("[INFO]: Solution: %lld\n", needle);
-  } else {
-    printf("[INFO]: Solution: no prime factor found\n");
-  }
+  printf("[INFO]: Solution: %d\n", target);
   return 0;
 }
